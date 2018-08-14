6d3e10d49a6b0eb4385a51b72a27e1e515a07e81
= <=>
2018-08-14 14:58:59 -0300

    Gsap en slider

--- src/app/componentes/body/body.component.ts
+++ src/app/componentes/body/body.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { QueHacemosService } from '../../services/que-hacemos.service';
 
+/*declare var jQuery: any;
+declare var $: any;
+declare var TweenMax: any;
+declare var Power2: any;*/
+
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
@@ -13,7 +18,12 @@ export class BodyComponent implements OnInit {
   constructor( private _qhs: QueHacemosService ) { }
 
   ngOnInit() {
-
+    /*const titulos: any = $('h5.card-title').css('opacity', 0);
+    const textos: any = $('p.card-text').css('opacity', 0);
+    const iconos: any = $('.material-icons').css('opacity', 0);*/
+    /*TweenMax.to( iconos, 0.8 , { opacity: 1, y: '+=10px', ease: Power2.easeInOut });
+    TweenMax.to( titulos, 0.8 , {  opacity: 1, delay: 0.6, y: '-=10px' , ease: Power2.easeInOut });
+    TweenMax.to( textos, 0.8 , {delay: 1, opacity: 1, y: '-=10px' , ease: Power2.easeInOut });*/
     this.data = this._qhs.getInfo();
   }
 
--- src/app/componentes/slider/slider.component.ts
+++ src/app/componentes/slider/slider.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Declaramos las variables para jQuery
+declare var jQuery: any;
+declare var $: any;
+declare var TweenMax: any;
+declare var Power2: any;
+
+
+
+
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
@@ -10,6 +19,19 @@ export class SliderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const Back: any = 'Elastic';
+
+    /*const mainNavbar: any = $('#mainNav').css('opacity', 0);*/
+    const titulos: any = $('h2').css('opacity', 0);
+    const textos: any = $('p').css('opacity', 0);
+    const btnArea: any = $('.btn-area').css('opacity', 0);
+
+  /*TweenMax.to( mainNavbar, 0.8 , {  opacity: 1, y: '+=10px' , ease:Power2.easeInOut });
+  TweenMax.from(mainNavbar, 0.8, { opacity: 1, ease: eas.easeInOut })*/
+  TweenMax.to( titulos, 0.8 , { delay: 1, opacity: 1, y: '-=10px' , ease: Back.easeOut });
+  TweenMax.to( textos, 0.8 , { delay: 1.5, opacity: 1, y: '-=10px' , ease: Back.easeOut });
+  TweenMax.to( btnArea, 0.8 , { delay: 2, opacity: 1, ease: Back.easeOut });
   }
+
 
 }
